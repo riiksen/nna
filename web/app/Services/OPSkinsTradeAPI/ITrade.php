08ab639f82322611ca5dfd26c6f2434cf0101ec0
Tim Fox <certexplay@gmail.com>
2018-12-13 14:51:20 +0100

    opskins trade api dir concept

---
+++ web/app/Services/OPSkinsTradeAPI/ITrade.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services\OPSkinsTradeAPI;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+trait ITrade {
+  public function cancelOffer() {
+
+  }
+
+  public function acceptOffer() {
+
+  }
+
+  public function getUserInventoryFromSteamId() {
+
+  }
+
+  public function senfOfferToSteamId() {
+
+  }
+}
