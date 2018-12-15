89face6524269c97fbcbc0bd1076700ee328e50a
Tim Fox <certexplay@gmail.com>
2018-12-15 19:06:31 +0100

    addded most needed api endpoints functions

--- web/app/Services/OPSkinsTradeAPI/IUser.php
+++ web/app/Services/OPSkinsTradeAPI/IUser.php
@@ -5,7 +5,24 @@ namespace App\Services\OPSkinsTradeAPI;
 use GuzzleHttp\Client as GuzzleClient;
 
 trait IUser {
-  public function getInventory($app_id, $options = []) {
+  public function getInventory($api_key, $data) {
+    $url = 'https://api-trade.opskins.com/IUser/GetInventory/v1/';
+    $method = 'GET';
 
+    $response = makeRequest($url, $method, $api_key, $data);
+
+    return $response;
+  }
+
+  private function makeRequest($url, $method, $api_key, $data) {
+    $client = new GuzzleHttp\Client();
+
+    $response = $client->request($method, $url, [
+      'headers' => [
+        'Authorization' => base64_encode($api_key)
+      ],
+
+      $method == 'POST' ? 'form_params' : 'query' => $data
+    ]);
   }
 }
