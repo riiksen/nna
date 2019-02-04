c46ccb9d2169b76b017d0302f17c4b1008e75823
Mike <certexplay@gmail.com>
2019-02-04 20:58:58 +0100

    wip - moving web to node

--- web-node/src/config/routes.ts
+++ web-node/src/config/routes.ts
@@ -1,5 +1,20 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
+
+import * as depositController from "../controllers/deposit.controller";
+import * as withdrawController from "../controllers/withdraw.controller";
+import config from "../config/config";
 
 const router = express.Router(); // eslint-disable-line new-cap
+
+// Withdraw routes
+router.get('/withdraw', withdrawController.index);
+router.get('/withdraw/:id', withdrawController.show);
+router.post('/withdraw', withdrawController.make);
+
+// Deposit routes
+router.get('/deposit', depositController.index);
+router.get('/deposit/:id', depositController.show);
+router.post('/deposit', depositController.make);
 
 export default router;
