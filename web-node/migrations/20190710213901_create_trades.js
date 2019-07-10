0571fb8f0420e1f9dc99090439feab50dbbfe7eb
Seweryn Starczyk <ksewosteam@gmail.com>
2019-07-10 23:24:29 +0200

    Remove empty space and added relationship Trades(user_id) => User(id)

--- web-node/migrations/20190710213901_create_trades.js
+++ web-node/migrations/20190710213901_create_trades.js
@@ -1,15 +1,17 @@
 exports.up = function(knex) {
   return Promise.all([  
-	  knex.schema.createTable('trades', function(table) {
-	    table.increments();
-	    table.integer('state');
-	    table.integer('recipent_id').unsigned();
-	    table.integer('value').unsigned();
-	    table.enum('type',['deposit','withdraw','jackpot_bet','coinflip_bet']);
-	    table.integer('offer_id').unsigned();
-	    table.string('trade_signature');
-	    table.timestamps();
-	  })
+    knex.schema.createTable('trades', function(table) {
+      table.increments();
+      table.integer('state');
+      table.integer('user_id').unsigned();
+      table.integer('value').unsigned();
+      table.enum('type',['deposit', 'withdraw', 'jackpot_bet', 'coinflip_bet']);
+      table.integer('offer_id').unsigned();
+      table.string('trade_signature');
+      table.timestamps();
+
+      table.foreign('user_id').references('id').inTable('users');
+    })
   ]);
 };
 
