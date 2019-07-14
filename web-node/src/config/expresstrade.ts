21e3a5f101d5e5d9aecc9161f1c206171306e7d6
Mike <certexplay@gmail.com>
2019-07-14 17:56:48 +0200

    commented out unused code

--- web-node/src/config/expresstrade.ts
+++ web-node/src/config/expresstrade.ts
@@ -11,28 +11,28 @@ const tradeManager = new ExpressTrade({
   pollInterval: 5000,
 });
 
-tradeManager.on('offerReceived', (offer: any) => {
-  tradeManager.ITrade.CancelOffer({ offer_id: offer.id });
-});
+// tradeManager.on('offerReceived', (offer: any) => {
+//   tradeManager.ITrade.CancelOffer({ offer_id: offer.id });
+// });
 
-// tradeManager.on('any', (event: any, offer: any) => {});
+// // tradeManager.on('any', (event: any, offer: any) => {});
 
-tradeManager.on('any', async (event: any, offer: any) => {
-  if (offer['sent_by_you'] && offer['state'] !== 2) {
-    const trade = await Trade.findByPk<Trade>(offer.id);
-    switch (offer.state) {
-      case 3: { // STATE_ACCEPTED
-        // log() // TODO(mike)
-        trade.finalize();
-      }
-      case 5: // STATE_EXPIRED
-      case 6: // STATE_CANCELED
-      case 7: // STATE_DECLINED
-      case 8: { // STATE_INVALID_ITEMS
+// tradeManager.on('any', async (event: any, offer: any) => {
+//   if (offer['sent_by_you'] && offer['state'] !== 2) {
+//     // const trade = await Trade.findByPk<Trade>(offer.id);
+//     switch (offer.state) {
+//       case 3: { // STATE_ACCEPTED
+//         // log() // TODO(mike)
+//         // trade.finalize();
+//       }
+//       case 5: // STATE_EXPIRED
+//       case 6: // STATE_CANCELED
+//       case 7: // STATE_DECLINED
+//       case 8: { // STATE_INVALID_ITEMS
 
-      }
-    }
-  }
-});
+//       }
+//     }
+//   }
+// });
 
 export { tradeManager };
