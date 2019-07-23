d732db32744ce4bdd6a2a5063be08096c61112f3
Seweryn Starczyk <ksewosteam@gmail.com>
2019-07-24 01:34:18 +0200

    Added cookie property to session and passed sessionSecret to cookieParser

--- src/config/express.ts
+++ src/config/express.ts
@@ -26,6 +26,7 @@ app.use(session({
   secret: config.sessionSecret,
   resave: true,
   saveUninitialized: true,
+  cookie: {path: '/', httpOnly: true, secure: false, maxAge:7*24*60*60*1000 },
 }));
 
 // Load passport middleware
@@ -35,14 +36,14 @@ app.use(passport.session());
 // Set up static folder and send index.html for all requests that don't have api in it
 app.use(express.static(path.join(__dirname, publicDir)));
 app.use(/^((?!(api)).)*/, (req: express.Request, res: express.Response): void => {
-  res.send(`${req.isAuthenticated()} <br/><br/>d ${JSON.stringify(req.user)}`);
+  res.send(`${req.isAuthenticated()} <br/><br/>${JSON.stringify(req.user)}`);
   //res.sendFile(path.join(__dirname, `${publicDir}index.html`));
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cookieParser());
+app.use(cookieParser(config.sessionSecret));
 app.use(compress());
 app.use(methodOverride());
 
