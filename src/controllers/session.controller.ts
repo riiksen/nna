cc934912ccd6c3bb07f0f784e7117d25291631a3
Seweryn Starczyk <ksewosteam@gmail.com>
2019-07-23 23:50:07 +0200

    Added steam authenticate

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -1,16 +1,20 @@
-// import * as passport from 'passport';
+import * as passport from 'passport';
 // import * as SteamStrategy from 'passport-steam';
 import { Request, Response } from 'express';
 // import { Request, Response, NextFunction } from 'express';
 
 // eslint-disable-next-line
 export function login(req: Request, res: Response) {
+  passport.authenticate('steam', () => {
 
+  })(req,res);
 }
 
 // eslint-disable-next-line
 export function handle(req: Request, res: Response) {
-
+  passport.authenticate('steam', {failureRedirect: '/'}, () => {
+    res.redirect('/');
+  })(req,res);
 }
 
 // eslint-disable-next-line
