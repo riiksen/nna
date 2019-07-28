99ed295296406a364690c7771df5209be63fd9c4
Mike <certexplay@gmail.com>
2019-07-28 16:30:15 +0200

    added tests for application helpers

--- test/helpers/application_helper.spec.ts
+++ test/helpers/application_helper.spec.ts
@@ -1,5 +1,9 @@
 import { expect } from '../utils';
 
+import * as applicationHelper from '../../src/helpers/application_helper';
+
+import config from '../../src/config/config';
+
 describe('Application Helper', (): void => {
   describe('#appProtocol', (): void => {
     it('it returns https if supported otherwise http', async (): Promise<void> => {
@@ -8,42 +12,56 @@ describe('Application Helper', (): void => {
   });
 
   describe('#apiUrlWithPortFor', (): void => {
-    it('', async (): Promise<void> => {
-
+    it('returns a correct api url with port', async (): Promise<void> => {
+      expect(applicationHelper.apiUrlWithPortFor('/test_url')).to.be.oneOf([
+        `http://${config.host}:${config.port}/api/test_url`,
+        `https://${config.host}:${config.port}/api/test_url`,
+      ]);
     });
   });
 
   describe('#urlWithPortFor', (): void => {
-    it('', async (): Promise<void> => {
-
+    it('returns a correct url with port', async (): Promise<void> => {
+      expect(applicationHelper.urlWithPortFor('/test_url')).to.be.oneOf([
+        `http://${config.host}:${config.port}/test_url`,
+        `https://${config.host}:${config.port}/test_url`,
+      ]);
     });
-
   });
 
   describe('#rootUrl', (): void => {
-    it('', async (): Promise<void> => {
-
+    it('returns a website url without any path', async (): Promise<void> => {
+      expect(applicationHelper.rootUrl()).to.be.oneOf([
+        `http://${config.host}`,
+        `https://${config.host}`,
+      ]);
     });
-
   });
 
   describe('#rootUrlWithPort', (): void => {
-    it('', async (): Promise<void> => {
-
+    it('returns a website url without any path with port', async (): Promise<void> => {
+      expect(applicationHelper.rootUrlWithPort()).to.be.oneOf([
+        `http://${config.host}:${config.port}`,
+        `https://${config.host}:${config.port}`,
+      ]);
     });
-
   });
 
   describe('#urlFor', (): void => {
-    it('', async (): Promise<void> => {
-
+    it('returns a correct url for given path', async (): Promise<void> => {
+      expect(applicationHelper.urlFor('/test_url')).to.be.oneOf([
+        `http://${config.host}/test_url`,
+        `https://${config.host}/test_url`,
+      ]);
     });
-
   });
 
   describe('#apiUrlFor', (): void => {
-    it('', async (): Promise<void> => {
-
+    it('it returns a correct api url for giver path', async (): Promise<void> => {
+      expect(applicationHelper.apiUrlFor('/test_url')).to.be.oneOf([
+        `http://${config.host}/api/test_url`,
+        `https://${config.host}/api/test_url`,
+      ]);
     });
   });
 });
