64a438bd1b630818bbe24e680edf07444c6bfb2e
Mike <certexplay@gmail.com>
2019-07-28 16:07:10 +0200

    added redirect to root after login

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 
 import passport from '../config/passport';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function login(req: Request, res: Response): void {
   const { provider } = req.params;
   switch (provider) {
@@ -15,14 +14,13 @@ export function login(req: Request, res: Response): void {
   }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function handle(req: Request, res: Response): void {
   const { provider } = req.params;
   switch (provider) {
     case 'opskins':
       break;
     case 'steam':
-      passport.authenticate('steam')(req, res, (): void => {});
+      passport.authenticate('steam')(req, res, (): void => res.redirect('/'));
       break;
     default:
   }
