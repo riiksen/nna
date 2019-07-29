25d0f5ba705ab2bfd0898b1236d81f79f3a5beb7
Mike <certexplay@gmail.com>
2019-07-29 12:08:01 +0200

    moved first migrations to typescript files and fixed style issues within them

---
+++ db/migrations/20190710213320_create_users.ts
@@ -0,0 +1,20 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.createTable('users', (t): void => {
+    t.increments();
+    t.string('username');
+    t.string('steamid');
+    t.integer('opskins_id').unsigned();
+    t.string('avatar');
+    t.integer('coins').unsigned();
+    t.boolean('locked');
+    t.boolean('admin');
+    t.boolean('in_trade');
+    t.timestamps();
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.dropTable('users');
+}
---
+++ db/migrations/20190710213901_create_trades.ts
@@ -0,0 +1,20 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.createTable('trades', (t): void => {
+    t.increments();
+    t.integer('state');
+    t.integer('user_id').unsigned();
+    t.integer('value').unsigned();
+    t.enum('type', ['deposit', 'withdraw', 'jackpot_bet', 'coinflip_bet']);
+    t.integer('offer_id').unsigned();
+    t.string('trade_signature');
+    t.timestamps();
+
+    t.foreign('user_id').references('id').inTable('users');
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.dropTable('trades');
+}
