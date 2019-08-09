91696ba382e7e3adc3ed0d7b057ff6db2e54b10f
Seweryn Starczyk <ksewosteam@gmail.com>
2019-08-09 23:21:32 +0200

    test for profile view

---
+++ test/profile.spec.ts
@@ -0,0 +1,19 @@
+import { expect, request } from './utils';
+
+import app from '../src/main';
+import * as passport from 'passport';
+import { Strategy as SteamStrategy } from 'passport-steam';
+
+describe('profile', (): void => {
+  describe('guest', () => {
+    it('should return redirect to: /', () => {
+
+    });
+  });
+  describe('user', () => {
+    
+    it('should display profile page', () => {
+
+    });
+  });
+});
