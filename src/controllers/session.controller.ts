f070a2da4a8455d33288ea841d480948b205a282
Mike <certexplay@gmail.com>
2019-08-18 16:49:48 +0200

    Refactoring session controller

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -2,23 +2,25 @@ import { Request, Response } from 'express';
 
 import passport from '../config/passport';
 
+const AVAILABLE_PROVIDERS = [
+  'steam',
+];
+
+function validProvider(provider: string): boolean {
+  return AVAILABLE_PROVIDERS.includes(provider);
+}
+
 export function login(req: Request, res: Response): void {
   const { provider } = req.params;
-  switch (provider) {
-    case 'steam':
-      passport.authenticate('steam')(req, res, (): void => {});
-      break;
-    default:
+  if (validProvider(provider)) {
+    passport.authenticate(provider)(req, res, (): void => {});
   }
 }
 
 export function handle(req: Request, res: Response): void {
   const { provider } = req.params;
-  switch (provider) {
-    case 'steam':
-      passport.authenticate('steam')(req, res, (): void => res.redirect('/'));
-      break;
-    default:
+  if (validProvider(provider)) {
+    passport.authenticate(provider)(req, res, (): void => res.redirect('/'));
   }
 }
 
