f8705b3f671a81cdedcf063cf2291a371ad98ba6
ksewo <you@example.com>
2019-10-09 21:49:28 +0200

    Added ForeignKeys for Role,User,Permission

--- src/models/role_permission.ts
+++ src/models/role_permission.ts
@@ -11,21 +11,26 @@ import {
   HasMany,
 } from 'sequelize-typescript';
 
+import Role from './role';
+import Permission from './permission';
+import User from './user';
+
 @Table({
   timestamps: true,
-  tableName: 'roles_permissions',
+  tableName: 'role_permissions',
   underscored: true,
 })
 export default class RolePermission extends Model<RolePermission> {
-  @PrimaryKey
-  @AutoIncrement
-  @Column
-  id!: number;
 
+  @PrimaryKey
   @Column
+  @ForeignKey((): typeof Role => Role)
+  @ForeignKey((): typeof User => User)
   roleId!: number;
 
+  @PrimaryKey
   @Column
+  @ForeignKey((): typeof Permission => Permission)
   permissionId!: number;
 
 }
