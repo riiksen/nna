74420ac09d21f68ce170488d8a7bef035ff0bb6d
ksewo <you@example.com>
2019-10-11 17:07:41 +0200

    Added return types

--- src/middlewares/role.ts
+++ src/middlewares/role.ts
@@ -2,9 +2,9 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../models/user';
 import Role from '../models/role';
 
-export default function (role: string) {
+export default function (role: string): void => {
   return function (req: Request, res: Response, next: NextFunction) {
-    User.findOne<User>({ where: { id: req.user.id }, include: [Role] }).then((user) => {
+    User.findOne<User>({ where: { id: req.user.id }, include: [Role] }).then((user): void => {
       if (user && user.isRole(role)) {
         return next();
       }
