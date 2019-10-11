e2f9f7e47752d56450404a8748c1aa0588a378a8
ksewo <you@example.com>
2019-10-11 15:57:04 +0200

    removed chai-passport-strategy

--- test/utils.ts
+++ test/utils.ts
@@ -1,9 +1,7 @@
 import * as chai from 'chai';
 
 import * as chaiHttp from 'chai-http';
-import * as chaiStrategy from 'chai-passport-strategy';
 
 chai.use(chaiHttp);
-chai.use(chaiStrategy);
 
 export { request, expect } from 'chai';
