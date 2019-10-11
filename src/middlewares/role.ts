8acf4b28aa1fc5d0c8076dad84afd469555e5077
ksewo <you@example.com>
2019-10-11 21:05:49 +0200

    Added typing to export default function

--- src/middlewares/role.ts
+++ src/middlewares/role.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../models/user';
 import Role from '../models/role';
 
-export default function (role: string) {
+export default function (role: string): (req: Request, res: Response, next: NextFunction) => void {
   return (req: Request, res: Response, next: NextFunction): void => {
     User.findOne<User>({ where: { id: req.user.id }, include: [Role] })
       .then((user): (Response | void) => {
