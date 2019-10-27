ffe0b0f7a03663f0068c8abdde40410bd48b15cf
ksewo <you@example.com>
2019-10-27 22:56:26 +0100

    Changed down function, so it reverts to previous value

--- db/migrations/20191012010844_change_type_to_int_trades.ts
+++ db/migrations/20191012010844_change_type_to_int_trades.ts
@@ -2,13 +2,12 @@ import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.table('trades', (table): void => {
-    table.dropColumn('type');
     table.integer('type');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.table('trades', (table): void => {
-    table.dropColumn('type');
+    table.enum('type', ['deposit', 'withdraw', 'jackpot_bet', 'coinflip_bet']);
   });
 }
