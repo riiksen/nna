b8641ad83b335d370780888004aa40cf6b6f9026
ksewo <you@example.com>
2019-10-27 23:10:13 +0100

    Weird fix, but okay

--- db/migrations/20191012010844_change_type_to_int_trades.ts
+++ db/migrations/20191012010844_change_type_to_int_trades.ts
@@ -2,11 +2,17 @@ import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.table('trades', (table): void => {
+    table.dropColumn('type');
+  });
+  await knex.schema.table('trades', (table): void => {
     table.integer('type');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table('trades', (table): void => {
+    table.dropColumn('type');
+  });
   await knex.schema.table('trades', (table): void => {
     table.enum('type', ['deposit', 'withdraw', 'jackpot_bet', 'coinflip_bet']);
   });
