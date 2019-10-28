d68de36f691ab22be45049c60c3727bc8bebed07
Mike <certexplay@gmail.com>
2019-10-29 00:02:09 +0100

    changed migration functions to be async

--- db/migrations/20190728023227_add_unique_to_steamid_in_users.ts
+++ db/migrations/20190728023227_add_unique_to_steamid_in_users.ts
@@ -1,13 +1,13 @@
 import * as Knex from 'knex';
 
-export function up(knex: Knex): any {
-  return knex.schema.table('users', (t): void => {
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table('users', (t): void => {
     t.unique(['steamid']);
   });
 }
 
-export function down(knex: Knex): any {
-  return knex.schema.table('users', (t): void => {
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table('users', (t): void => {
     t.dropUnique(['steamid']);
   });
 }
--- db/migrations/20190728232935_remove_opskins_from_users.ts
+++ db/migrations/20190728232935_remove_opskins_from_users.ts
@@ -1,13 +1,13 @@
 import * as Knex from 'knex';
 
-export function up(knex: Knex): any {
-  return knex.schema.table('users', (t): void => {
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table('users', (t): void => {
     t.dropColumn('opskins_id');
   });
 }
 
-export function down(knex: Knex): any {
-  return knex.schema.table('users', (t): void => {
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table('users', (t): void => {
     t.integer('opskins_id').unsigned();
   });
 }
