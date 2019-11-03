9398f4cd972edac7072660e5ca98c7d1095adb98
ksewo <you@example.com>
2019-11-03 15:33:49 +0100

    Added disconnect socket in correct jest function, added afterAll with server.close(), and creating server in beforeAll instead of beforeEach

--- test/socket.spec.ts
+++ test/socket.spec.ts
@@ -7,27 +7,41 @@ import config from '../src/config/config';
 
 describe('Socket', (): void => {
   let socket: SocketIOClient.Socket;
+  let server: http.Server | null = null;
 
-  beforeEach((done): void => {
-    if (socketServer() === null) {
-      const server = http.createServer();
+  beforeAll((done): void => {
+    if(socketServer() === null) {
+      server = http.createServer();
+
       initializeSocket(server);
 
       server.listen(config.port);
     }
+    done();
+  });
 
+  beforeEach((done): void => {
     socket = io(applicationHelper.rootUrlWithPort());
     done();
   });
+  
   it('Socket client should connect to socket server', async(done): Promise<void> => {
     socket.on('connect', (): void => {
       expect(socket.connected).to.equal(true);
       done();
     });
   });
+  
   afterEach((done): void => {
-    if (socket.connected) {
+    if(socket.connected) {
       socket.disconnect();
+    }
+    done();
+  });
+  
+  afterAll((done): void => {
+    if(server !== null) {
+      server.close();
     }
     done();
   });
