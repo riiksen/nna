b143032d442ceca5b47d90b60687e1cdc17b7575
ksewo <you@example.com>
2019-11-03 15:09:07 +0100

    Create socket server if does not exist

--- test/socket.spec.ts
+++ test/socket.spec.ts
@@ -1,11 +1,21 @@
 import * as io from 'socket.io-client';
 import { expect } from './utils';
 import * as applicationHelper from '../src/helpers/application.helper';
+import { io as socketServer, initialize as initializeSocket } from '../src/config/socket';
+import * as http from 'http';
+import config from '../src/config/config';
 
 describe('Socket', (): void => {
   let socket: SocketIOClient.Socket;
 
   beforeEach((done): void => {
+    if(socketServer() === null) {
+      const server = http.createServer();
+      initializeSocket(server);
+      
+      server.listen(config.port);
+    }
+
     socket = io(applicationHelper.rootUrlWithPort());
     done();
   });
