79d06a79caf7663827cbfaeaa17b1716ec651532
ksewo <you@example.com>
2019-11-03 15:13:17 +0100

    .

--- test/socket.spec.ts
+++ test/socket.spec.ts
@@ -1,18 +1,18 @@
 import * as io from 'socket.io-client';
+import * as http from 'http';
 import { expect } from './utils';
 import * as applicationHelper from '../src/helpers/application.helper';
 import { io as socketServer, initialize as initializeSocket } from '../src/config/socket';
-import * as http from 'http';
 import config from '../src/config/config';
 
 describe('Socket', (): void => {
   let socket: SocketIOClient.Socket;
 
   beforeEach((done): void => {
-    if(socketServer() === null) {
+    if (socketServer() === null) {
       const server = http.createServer();
       initializeSocket(server);
-      
+
       server.listen(config.port);
     }
 
