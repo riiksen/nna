54315cd4c301710d5fb8148cb46926c0b3ace2c6
ksewo <you@example.com>
2019-11-03 15:44:32 +0100

    put beforeAll and afterAll outside describe

--- test/socket.spec.ts
+++ test/socket.spec.ts
@@ -5,44 +5,45 @@ import * as applicationHelper from '../src/helpers/application.helper';
 import { io as socketServer, initialize as initializeSocket } from '../src/config/socket';
 import config from '../src/config/config';
 
+let server: http.Server | null = null;
+
+beforeAll((done): void => {
+  if (socketServer() === null) {
+    server = http.createServer();
+
+    initializeSocket(server);
+
+    server.listen(config.port);
+  }
+  done();
+});
+
 describe('Socket', (): void => {
   let socket: SocketIOClient.Socket;
-  let server: http.Server | null = null;
-
-  beforeAll((done): void => {
-    if(socketServer() === null) {
-      server = http.createServer();
-
-      initializeSocket(server);
-
-      server.listen(config.port);
-    }
-    done();
-  });
 
   beforeEach((done): void => {
     socket = io(applicationHelper.rootUrlWithPort());
     done();
   });
-  
-  it('Socket client should connect to socket server', async(done): Promise<void> => {
+
+  it('Socket client should connect to socket server', async (done): Promise<void> => {
     socket.on('connect', (): void => {
       expect(socket.connected).to.equal(true);
       done();
     });
   });
-  
+
   afterEach((done): void => {
-    if(socket.connected) {
+    if (socket.connected) {
       socket.disconnect();
     }
     done();
   });
-  
-  afterAll((done): void => {
-    if(server !== null) {
-      server.close();
-    }
-    done();
-  });
+});
+
+afterAll((done): void => {
+  if (server !== null) {
+    server.close();
+  }
+  done();
 });
