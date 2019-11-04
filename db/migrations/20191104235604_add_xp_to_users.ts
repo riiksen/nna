7f73fb21be46d45faff6aaa747ac4b8aed8f9a6f
ksewo <you@example.com>
2019-11-05 00:08:01 +0100

    Created migration to add xp column to user

---
+++ db/migrations/20191104235604_add_xp_to_users.ts
@@ -0,0 +1,14 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.table('users', (table): void => {
+    table.integer('xp').unsigned();
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.table('users', (table): void => {
+    table.dropColumn('xp');
+  });
+}
