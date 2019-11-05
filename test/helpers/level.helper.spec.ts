7dfe3b6029051203e7d0faf11970d9c91ed7f960
ksewo <you@example.com>
2019-11-05 20:53:03 +0100

    Fixed levelHelper functions placed in wrong place

--- test/helpers/level.helper.spec.ts
+++ test/helpers/level.helper.spec.ts
@@ -3,17 +3,17 @@ import settings from '../../src/config/settings';
 import { expect } from '../utils';
 
 describe('Level Helper', (): void => {
-  const firstLevelXp = settings.coins_to_usd_rate * settings.levels.multiplier
+  const firstLevelXp = settings.coins_to_usd_rate * settings.levels.multiplier;
 
   describe('#getXp', (): void => {
     it('Return xp from level', (): void => {
-      expect(levelHelper.getLevel(1)).to.equal(firstLevelXp);
+      expect(levelHelper.getXp(1)).to.equal(firstLevelXp);
     });
   });
 
   describe('#getLevel', (): void => {
     it('Return level from xp', (): void => {
-      expect(levelHelper.getXp(firstLevelXp)).to.equal(1);
+      expect(levelHelper.getLevel(firstLevelXp)).to.equal(1);
     });
   });
 });
