0723394b0c2efde85c75a67164aea0f6258efc5e
ksewo <you@example.com>
2019-11-05 21:13:45 +0100

    Added return type to function

--- test/helpers/math.helper.spec.ts
+++ test/helpers/math.helper.spec.ts
@@ -4,7 +4,7 @@ import * as mathHelper from '../../src/helpers/math.helper';
 
 describe('Math Helper', (): void => {
   describe('#mathClmap', (): void => {
-    it('Returns clamped value', () => {
+    it('Returns clamped value', (): void => {
       expect(mathHelper.clamp(10, 1, 5)).to.equal(5);
       expect(mathHelper.clamp(-2, 1, 5)).to.equal(1);
       expect(mathHelper.clamp(3, 1, 5)).to.equal(3);
