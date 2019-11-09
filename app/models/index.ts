c5ca38777047762cfcab65df243955f0ea25b74a
Mike <certexplay@gmail.com>
2019-11-09 23:38:21 +0100

    exported all the models in models/index.ts

---
+++ app/models/index.ts
@@ -0,0 +1,5 @@
+export { default as Permission } from './permission';
+export { default as Role } from './role';
+export { default as RolePermission } from './role_permission';
+export { default as Trade } from './trade';
+export { default as User } from './user';
