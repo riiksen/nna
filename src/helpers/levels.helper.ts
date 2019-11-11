20620f469d0fcdc01595117c184245d42b754f70
ksewo <you@example.com>
2019-11-11 18:39:33 +0100

    Changed levelsHelper, so minimum level is 1 now, added maxLevelXp const

--- src/helpers/levels.helper.ts
+++ src/helpers/levels.helper.ts
@@ -1,8 +1,14 @@
 import { clamp } from './math.helper';
 import settings from '../config/settings';
 
+const maxLevelXp = Math.floor((settings.levels.multiplier ** (settings.levels.max - 1)) * settings.coinsToUSDRate);
+
 export function getXp(level: number): number {
-  return Math.floor((settings.levels.multiplier ** level) * settings.coinsToUSDRate);
+  if(level <= 1) return 0;
+
+  const xp = Math.floor((settings.levels.multiplier ** (level - 1)) * settings.coinsToUSDRate);
+
+  return clamp(xp, 0, maxLevelXp);
 }
 
 export function getXpToNextLevel(level: number): number {
@@ -13,7 +19,9 @@ export function getXpToNextLevel(level: number): number {
 
 export function getLevel(xp: number): number {
   const level = Math.floor(Math.log(xp / settings.coinsToUSDRate)
-    / Math.log(settings.levels.multiplier));
+    / Math.log(settings.levels.multiplier)) + 1;
 
-  return clamp(level, 0, settings.levels.max);
+  return clamp(level, 1, settings.levels.max);
 }
+
+export { maxLevelXp };
