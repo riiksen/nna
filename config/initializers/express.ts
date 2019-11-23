46ac9a80681b718b60adfaaac164b19ee601304d
Mike <certexplay@gmail.com>
2019-11-23 13:06:51 +0100

    removed unsued session related imports

--- config/initializers/express.ts
+++ config/initializers/express.ts
@@ -1,13 +1,11 @@
 import * as bodyParser from 'body-parser';
 import * as compress from 'compression';
-import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors';
 import * as express from 'express';
 import * as helmet from 'helmet';
 // import * as httpError from 'http-errors';
 import * as logger from 'morgan';
 import * as methodOverride from 'method-override';
-import * as session from 'express-session';
 
 import { config } from '../config';
 import { passport } from './passport';
