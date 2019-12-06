e1b42d723318f279b967d60fee8789013f8cda29
ksewo <you@example.com>
2019-12-06 20:00:41 +0100

    Added protection against using refresh token as access token

--- config/initializers/passport.ts
+++ config/initializers/passport.ts
@@ -44,6 +44,9 @@ passport.use(new JWTStrategy({
   done: DoneFunction<User>,
 ): Promise<void> => {
   try {
+    if(payload.isRefreshToken) {
+      return done(new Error('Can not use refresh token as access token'));
+    }
     const user = await User.findByPk<User>(payload.id);
     if (user) {
       done(null, user);
